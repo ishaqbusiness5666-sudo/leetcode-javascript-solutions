8042154f33c64acfb46b6e91190a1b077d55cb91
Muhammad Ishaque <ishaqbusiness5666@gmail.com>
2026-06-18 20:36:33 +0500

    solve the Two-Sum problem

--- Arrays/TwoSum.js
+++ Arrays/TwoSum.js
@@ -37,6 +37,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  
+    const numMap = new Map(); // Map to store the indices of the numbers
+     for (let i = 0; i < nums.length; i++) {  // Iterate through the array
+        const complement = target - nums[i];  // Calculate the complement
+        if (numMap.has(complement)) {   // Check if the complement exists in the map
+            return [numMap.get(complement), i]; // If it exists, return the indices of the complement and the current number
+        } 
+        numMap.set(nums[i], i);// If the complement does not exist, add the current number and its index to the map
+    }
     
 };
+
+console.log(twoSum([2,7,11,15], 9)); // Output: [0, 1]
+console.log(twoSum([3,2,4], 6)); // Output: [1, 2]
+console.log(twoSum([3,3], 6)); // Output: [0, 1]
