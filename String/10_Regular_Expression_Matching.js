1366cbeb64e0af08221043db4cf0841b6d6237d7
MUHAMMAD-ISHAQUE <ishaqbusiness5666@gmail.com>
2026-06-27 23:42:19 +0500

    Add problem 12 Integer To String

---
+++ String/10_Regular_Expression_Matching.js
@@ -0,0 +1,24 @@
+// /**
+//  * @param {string} s
+//  * @param {string} p
+//  * @return {boolean}
+//  */
+// const isMatch = function(s, p) {
+    
+// };
+
+// let s = "*aa", p = "a";
+
+// console.log(OutPattern("a.a"))
+
+// function OutPattern(String){
+// for (let ch of String) {
+//     if ((ch >= '*' && ch <= '.')) {
+//         return ch
+//     }
+// }
+// return 0;
+// }
+
+
+// padding
